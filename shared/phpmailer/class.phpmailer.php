1681d4354dd1285b7c76fc66f2f3ba221c2858a4
nick <nick@nickworkstation.(none)>
2010-05-19 18:24:47 +0200

    10.1.004

--- shared/phpmailer/class.phpmailer.php
+++ shared/phpmailer/class.phpmailer.php
@@ -41,8 +41,15 @@
 if (version_compare(PHP_VERSION, '5.0.0', '<') ) exit("Sorry, this version of PHPMailer will only run on PHP version 5 or greater!\n");
 
 /**
+ * PHPMailer - PHP email transport class
+ * NOTE: Requires PHP version 5 or later
+ * @package PHPMailer
+ * @author Andy Prevost
+ * @author Marcus Bointon
+ * @copyright 2004 - 2009 Andy Prevost
+ * @version $Id: class.phpmailer.php 447 2009-05-25 01:36:38Z codeworxtech $
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
-
 class PHPMailer {
 
   /////////////////////////////////////////////////
