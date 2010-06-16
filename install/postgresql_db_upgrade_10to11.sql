4759a7b91ac8357081d752f2e166c45f15a1e3b7
nick <nick@nickworkstation.(none)>
2010-06-16 16:34:27 +0200

    11.0.000

---
+++ install/postgresql_db_upgrade_10to11.sql
@@ -0,0 +1,47 @@
+/*
+============================================================
+File name   : postgresql_db_upgrade_10to11.sql
+Begin       : 2010-06-16
+Last Update : 2010-06-16
+
+Description : TCExam database structure upgrade commands
+              (from version 10 to 11).
+Database    : PostgreSQL 8+
+
+Author: Nicola Asuni
+
+(c) Copyright:
+              Nicola Asuni
+              Tecnick.com S.r.l.
+              Via della Pace, 11
+              09044 Quartucciu (CA)
+              ITALY
+              www.tecnick.com
+              info@tecnick.com
+
+License:
+   Copyright (C) 2004-2010 Nicola Asuni - Tecnick.com S.r.l.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Additionally, you can't remove, move or hide the original TCExam logo,
+   copyrights statements and links to Tecnick.com and TCExam websites.
+
+   See LICENSE.TXT file for more information.
+//============================================================+
+*/
+
+ALTER TABLE "tce_modules" ADD "module_user_id" Bigint NOT NULL Default 1;
+ALTER TABLE "tce_modules" ADD Constraint "rel_module_author" foreign key ("module_user_id") references "tce_users" ("user_id") ON DELETE cascade;
+
