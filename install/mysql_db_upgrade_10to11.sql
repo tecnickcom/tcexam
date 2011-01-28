d9294ada210411b2fa6dd1be1da362f03313f981
Nick <nick@nickmain.(none)>
2011-01-28 18:43:54 +0100

    11.1.016

--- install/mysql_db_upgrade_10to11.sql
+++ install/mysql_db_upgrade_10to11.sql
@@ -2,7 +2,7 @@
 ============================================================
 File name   : mysql_db_upgrade_10to11.sql
 Begin       : 2010-06-16
-Last Update : 2010-06-16
+Last Update : 2011-01-28
 
 Description : TCExam database structure upgrade commands
               (from version 10 to 11).
@@ -45,4 +45,6 @@ License:
 ALTER TABLE tce_modules ADD module_user_id Bigint UNSIGNED NOT NULL DEFAULT 1;
 ALTER TABLE tce_modules ADD INDEX p_module_user_id (module_user_id);
 ALTER TABLE tce_modules ADD Foreign Key (module_user_id) references tce_users (user_id) ON DELETE cascade ON UPDATE no action;
+ALTER TABLE tce_questions DROP INDEX ak_question;
+ALTER TABLE tce_answers DROP INDEX ak_answer;
 
--- install/postgresql_db_upgrade_10to11.sql
+++ install/postgresql_db_upgrade_10to11.sql
@@ -2,7 +2,7 @@
 ============================================================
 File name   : postgresql_db_upgrade_10to11.sql
 Begin       : 2010-06-16
-Last Update : 2010-06-16
+Last Update : 2011-01-28
 
 Description : TCExam database structure upgrade commands
               (from version 10 to 11).
@@ -44,4 +44,5 @@ License:
 
 ALTER TABLE "tce_modules" ADD "module_user_id" Bigint NOT NULL Default 1;
 ALTER TABLE "tce_modules" ADD Constraint "rel_module_author" foreign key ("module_user_id") references "tce_users" ("user_id") ON DELETE cascade;
-
+ALTER TABLE "tce_questions" DROP INDEX "ak_question";
+ALTER TABLE "tce_answers" DROP INDEX "ak_answer";
