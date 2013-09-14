f882b1409e71a12723cf2d9b2f27537d3efd21a2
tecnickcom <info@tecnik.com>
2013-09-14 21:09:59 +0100

    12.1.007 (2013-09-14) - TCPDF Library was updated.

--- admin/code/index.php
+++ admin/code/index.php
@@ -16,23 +16,6 @@
 //
 // License:
 //    Copyright (C) 2004-2012  Nicola Asuni - Tecnick.com LTD
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Affero General Public License as
-//    published by the Free Software Foundation, either version 3 of the
-//    License, or (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Affero General Public License for more details.
-//
-//    You should have received a copy of the GNU Affero General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//    Additionally, you can't remove, move or hide the original TCExam logo,
-//    copyrights statements and links to Tecnick.com and TCExam websites.
-//
 //    See LICENSE.TXT file for more information.
 //============================================================+
 
@@ -53,11 +36,11 @@ $pagelevel = K_AUTH_INDEX;
 require_once('../../shared/code/tce_authorization.php');
 require_once('tce_page_header.php');
 
-echo '<div style="direction:ltr;text-align:left;border:1px solid black; padding:5px; margin:10px; background-color:#DDEEFF; color:#000000; width:95%; margin-left:auto; margin-right:auto; font-weight:bold; font-size:95%;">TCEXAM REQUIRES A COMMERCIAL LICENSE FOR ANY COMMERCIAL USE. PLEASE CONTACT <a href="mailto:info@tecnick.com" title="mail to tecnick.com">info@tecnick.com</a> FOR PURCHASING A LICENSE.</div>'.K_NEWLINE;
+echo '<div style="direction:ltr;text-align:left;border:1px solid black; padding:5px; margin:10px; background-color:#DDEEFF; color:#000000; width:95%; margin-left:auto; margin-right:auto; font-weight:bold; font-size:95%;">TCEXAM REQUIRES A COMMERCIAL LICENSE FOR ANY COMMERCIAL USE, INCLUDING WHEN A NON-PROFIT ORGANIZATION IS ASKING ANY FEE TO ITS MEMBERS/USERS, YOU MUST CONTACT <a href="mailto:info@tecnick.com" title="mail to tecnick.com">info@tecnick.com</a> FOR PURCHASING A LICENSE.</div>'.K_NEWLINE;
 
-echo '<div style="direction:ltr;text-align:left;border:1px solid black; padding:5px; margin:10px; background-color:#DDEEFF; color:#000000; width:95%; margin-left:auto; margin-right:auto; font-weight:bold; font-size:95%;">YOU CAN\'T REMOVE, MOVE OR HIDE THE ORIGINAL TCEXAM LOGO, COPYRIGHTS STATEMENTS AND LINKS TO <a href="http://www.tecnick.com" title="External link to Tecnick.com">TECNICK.COM</a> AND <a href="http://www.tcexam.org" title="External link to TCExam">TCEXAM</a> WEBSITES FROM THIS SOFTWARE.<br />THE FREE NON-COMMERCIAL TCEXAM LICENSE IS SUBJECT TO THE <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html" title="External link to GNU Affero General Public License">GNU-AGPL v.3 LICENSE.</a></div>'.K_NEWLINE;
+echo '<div style="direction:ltr;text-align:left;border:1px solid black; padding:5px; margin:10px; background-color:#DDEEFF; color:#000000; width:95%; margin-left:auto; margin-right:auto; font-weight:bold; font-size:95%;">THE FREE NON-COMMERCIAL TCEXAM LICENSE IS SUBJECT TO THE <a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html" title="External link to GNU Affero General Public License">GNU-AGPL v.3 LICENSE</a> WITH ADDITIONAL RESTRICTIONS: YOU CAN\'T REMOVE, MOVE OR HIDE THE ORIGINAL TCEXAM LOGO, COPYRIGHTS STATEMENTS AND LINKS TO <a href="http://www.tecnick.com" title="External link to Tecnick.com">TECNICK.COM</a> AND <a href="http://www.tcexam.org" title="External link to TCExam">TCEXAM</a> WEBSITES FROM THIS SOFTWARE.<br /></div>'.K_NEWLINE;
 
-echo '<div style="direction:ltr;text-align:left;border:1px solid black; padding:5px; margin:10px; background-color:#DDEEFF; color:#000000; width:95%; margin-left:auto; margin-right:auto; font-weight:bold; font-size:95%;">PLEASE SUPPORT TCEXAM <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=NZUEC5XS8MFBJ" title="External link to make a donation for TCExam">MAKING A DONATION</a>.</div>'.K_NEWLINE;
+echo '<div style="direction:ltr;text-align:left;border:1px solid black; padding:5px; margin:10px; background-color:#DDEEFF; color:#000000; width:95%; margin-left:auto; margin-right:auto; font-weight:bold; font-size:95%;">IF YOU ARE USING THE FREE VERSION PLEASE CONSIDER SUPPORTING THE TCEXAM PROJECT BY <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=NZUEC5XS8MFBJ" title="External link to make a donation for TCExam">MAKING A DONATION</a>.</div>'.K_NEWLINE;
 
 // Display test limits (if any)
 
--- shared/config.default/tce_config.php
+++ shared/config.default/tce_config.php
@@ -17,23 +17,6 @@
 //
 // License:
 //    Copyright (C) 2004-2013 Nicola Asuni - Tecnick.com LTD
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Affero General Public License as
-//    published by the Free Software Foundation, either version 3 of the
-//    License, or (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Affero General Public License for more details.
-//
-//    You should have received a copy of the GNU Affero General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//    Additionally, you can't remove, move or hide the original TCExam logo,
-//    copyrights statements and links to Tecnick.com and TCExam websites.
-//
 //    See LICENSE.TXT file for more information.
 //============================================================+
 
@@ -49,7 +32,7 @@
 /**
  * TCExam version (do not change).
  */
-define ('K_TCEXAM_VERSION', '12.1.006');
+define ('K_TCEXAM_VERSION', '12.1.007');
 
 /**
  * 2-letters code for default language.
