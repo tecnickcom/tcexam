002d5a2ba128baaacd47e79911d5c77202765d5c
Damilola Olowookere <damms005@gmail.com>
2019-12-18 10:24:35 +0100

    display helpful message when unable to connect to post-installation database

--- install/tce_functions_install.php
+++ install/tce_functions_install.php
@@ -215,6 +215,7 @@ function F_create_database($dbtype, $host, $port, $user, $password, $database, $
 	if ($db = @F_db_connect($host, $port, $user, $password, $database)) {
 		return $db;
 	} else {
+		echo "<span style='color:#000080'>[could not access post-installation database: (host:{$host}, port:{$port}, user:{$user}, password:{$password}, database:{$database})]" . F_db_error($db) . "</span>";
 		return FALSE;
 	}
 }
