f463bca464c738e246b5b709d3c95d18de02e68a
mamans86 <48341998+mamans86@users.noreply.github.com>
2020-07-05 00:41:32 +0700

    Add conditional for theme

--- public/code/tce_page_footer.php
+++ public/code/tce_page_footer.php
@@ -36,6 +36,9 @@ echo '</div>'.K_NEWLINE; //close div.body
 include('../../shared/code/tce_page_userbar.php'); // display user bar
 
 echo '<!-- '.base64_decode(K_KEY_SECURITY).' -->'.K_NEWLINE;
+if(K_PUBLIC_THEME!='default'){
+	include(K_PATH_JSCRIPTS.'additional/'.K_PUBLIC_THEME.'/index.php');
+}
 echo '</body>'.K_NEWLINE;
 echo '</html>';
 
